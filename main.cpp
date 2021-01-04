a8cecc7203adadf9a15086da3c033228d31ec093
Hyunmin Jung <jung@hyunmin.dev>
2021-01-04 10:55:12 +0900

    Make adjacency matrix

--- main.cpp
+++ main.cpp
@@ -9,39 +9,55 @@ struct Position {
   int y;
 };
 
+int position_number;
 vector<Position> position_vec;
+vector<vector<int>> position_graph;
 
-bool isMovable(Position start_position, Position arrival_position, int health) {
+void makeGraph() {
+  for (int i = 0; i < position_vec.size(); i++) {
+    for (int j = 0; j < position_vec.size(); j++) {
+      int x_distance = abs(position_vec[i].x - position_vec[j].x);
+      int y_distance = abs(position_vec[i].y - position_vec[j].y);
+      position_graph[i][j] = min(x_distance, y_distance);
+      position_graph[j][i] = min(x_distance, y_distance);
+    }
+  }
+}
 
-  return true;
+int getDistance(int start_index, int arrival_index) {
+  if (start_index == arrival_index) {
+    return 0;
+  }
+
+  int distance = 0;
+  for (int i = 0; i < position_number; i++) {
+    distance += getDistance(i, arrival_index);
+  }
+  return distance;
 }
 
 int main() {
-  int position_number;
   int question_number;
   cin >> position_number >> question_number;
 
-  position_vec.resize(position_number);
-  vector<bool> result_vec(question_number);
+  position_vec.resize(position_number); // resize global variable
+  position_graph.resize(position_number, vector<int>(position_number));
+  vector<bool> result_vec(question_number); // initialize result vector
 
   for (int i = 0; i < position_number; i++) {
     cin >> position_vec[i].x >> position_vec[i].y;
   }
 
+  makeGraph();
+
   for (int i = 0; i < question_number; i++) {
-    int start;
-    int arrival;
-    int health;
+    int start, arrival, health;
     cin >> start >> arrival >> health;
-    result_vec[i] = isMovable(position_vec[start - 1], position_vec[arrival - 1], health);
+    result_vec[i] = getDistance(start - 1, arrival - 1) <= health;
   }
 
   for (bool result : result_vec) {
-    if (result) {
-      cout << "YES" << endl;
-    } else {
-      cout << "NO" << endl;
-    }
+    result ? cout << "YES" << endl : cout << "NO" << endl;
   }
 
   return 0;
